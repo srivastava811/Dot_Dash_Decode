64d967da5a590f81d6a6956576dde55a1223d983
srivastava811 <srivastavatulika0811@gmail.com>
2025-01-01 09:43:08 +0530

    calculation of Eye Aspect Ratio(EAR)

--- computer_vision/blink_detection.py
+++ computer_vision/blink_detection.py
@@ -17,3 +17,16 @@ predictor=dlib.shape_predictor("shape_predictor_68__face_landmarks.dat")
 
 open=0
 closed=0
+
+# calculation of euclidean distance between the points around the eyes
+
+def distance(pa,pb):
+    dist=np.linalg.norm(pa,pb)
+    return dist
+
+# calculate EAR (Eye Aspect Ratio)
+
+def eye_aspect_ratio(a,b,c,d,e,f):
+    horizontal_dist=distance(b,d)+distance(c,e)
+    vertical_dist=distance(a,f)
+    ear=horizontal_dist/(2.0*vertical_dist)
