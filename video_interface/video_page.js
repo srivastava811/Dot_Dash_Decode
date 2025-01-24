fd2b89f168a7ae98813bb9d6818cd6b779fec4e0
Deepanshi Gupta <133107673+DEEPXANSHI@users.noreply.github.com>
2025-01-24 19:28:08 +0530

    Add files via upload

---
+++ video_interface/video_page.js
@@ -0,0 +1,12 @@
+let v = document.querySelector("#videoElement");
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            v.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.log("Something went wrong:", error);
+        });
+} else {
+    console.log("getUserMedia not supported!");
+}
